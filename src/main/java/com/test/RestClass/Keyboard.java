a9cbd4852a109b87705e45502b59dbc63173f7ea
Emrys <32980429+GodfatherEmrys@users.noreply.github.com>
2018-01-26 22:41:18 +0900

    Rest API Test

---
+++ src/main/java/com/test/RestClass/Keyboard.java
@@ -0,0 +1,14 @@
+package com.test.RestClass;
+
+public class Keyboard {
+
+    private final String type;
+
+    public Keyboard(){
+        this.type = "text";
+    }
+
+    public String getType(){
+        return type;
+    }
+}
---
+++ src/main/java/com/test/hello/RestTestController.java
@@ -0,0 +1,17 @@
+package com.test.hello;
+
+import com.test.RestClass.Keyboard;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class RestTestController {
+
+    @RequestMapping("/keyboard")
+    public Keyboard getKeyboard(){
+        return new Keyboard();
+    }
+}
