f8b9a206e6d40894d9e7d9a0feee55150c8ac7d0
Emrys <32980429+GodfatherEmrys@users.noreply.github.com>
2018-01-26 23:58:30 +0900

    Plus Friend Accept

--- src/main/java/com/test/RestClass/Keyboard.java
+++ src/main/java/com/test/RestClass/Keyboard.java
@@ -2,13 +2,18 @@ package com.test.RestClass;
 
 public class Keyboard {
 
-    private final String type;
+    private String type;
+    private String[] buttons;
 
     public Keyboard(){
-        this.type = "text";
+        this.type = "buttons";
+        this.buttons = new String[]{"선택1", "선택2", "선택3"};
     }
 
     public String getType(){
         return type;
+    }
+    public String[] getButtons(){
+        return buttons;
     }
 }
--- src/main/java/com/test/hello/RestTestController.java
+++ src/main/java/com/test/hello/RestTestController.java
@@ -3,6 +3,7 @@ package com.test.hello;
 import com.test.RestClass.Keyboard;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
