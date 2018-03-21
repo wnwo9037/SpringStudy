8402b646b582a96993f72544d8405b67ec714a03
wnwo <wnwo9037@gmail.com>
2018-03-21 23:05:37 +0900

    Redis, Jooq 해봄

---
+++ src/main/java/com/test/config/ClusterConfigurationProperties.java
@@ -0,0 +1,27 @@
+package com.test.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis.cluster")
+public class ClusterConfigurationProperties {
+
+    List<String> nodes;
+
+    /**
+     * Get initial collection of known cluster nodes in format {@code host:port}.
+     *
+     * @return
+     */
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<String> nodes) {
+        this.nodes = nodes;
+    }
+}
+
--- src/main/java/com/test/kakaobot/SampleController.java
+++ src/main/java/com/test/kakaobot/SampleController.java
@@ -4,21 +4,31 @@ import com.test.mappers.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.util.JedisClusterCRC16;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-
     @Autowired
     private TestMapper testMapper;
+    @Autowired
+    RedisConnectionFactory connectionFactory;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     String home() {
+        RedisClusterConnection connection = connectionFactory.getClusterConnection();
+        for (int i=0; i<1000; ++i) {
+            String key = "foo" + i;
+            System.out.println("key: " +  key + ", slot: " + JedisClusterCRC16.getCRC16(key.getBytes()));
+            connection.set(key.getBytes(), "bar".getBytes());
+        }
         return "Hello World!";
     }
 
